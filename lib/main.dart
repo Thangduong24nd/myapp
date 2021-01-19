16b3232445f71430188375b60b891c4dfa1d9d2f
Thang Duong <73693556+Thangduong24nd@users.noreply.github.com>
2021-01-19 16:01:44 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  //viết, nạp, gọi các phương thức cần thiết trước khi runapp
+  runApp(
+    Center(
+      child: Text(
+        'Hello Thang',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
